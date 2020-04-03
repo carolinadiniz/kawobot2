f8d83c8ef8ded02dc83fc5f6df6ffca3114df60d
Carolina Diniz (Kaworii) <phdsd.phdsd@gmail.com>
2020-04-03 02:21:51 -0300

    removed var mongoose and create a new color when connect to tmi

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 // importando modulos
 const tmi = require('tmi.js')
-const mongoose = require('mongoose')
 const connection = require('./settings/connection.js')
 const commands = require('./commands/commands.js')
 const connection_mongoose = require('./settings/connection_mongoose')
@@ -12,7 +11,10 @@ connection_mongoose()
 
 // TMI.js
 var client = new tmi.client(connection)
-client.connect()
+client.connect().then(() => 
+    console.log('\033[0;32m[\033[0;36m'+'tmi.js \033[0;32m Conectado com Sucesso]'+'\033[0m')).catch(() =>
+    console.log('\033[0;31m[\033[0;36m'+'tmi.js \033[0;31mFalha ao tentar se conectar]'+'\033[0m')
+)
 
 client.on('chat', function (channel, username, message, self) {
    new commands(client, channel, username, message, self)
